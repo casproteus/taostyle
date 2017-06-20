4f1b608a1dee70a36ede0b7d34f658afc1f2b892
tao <casproteus@hotmail.com>
2017-06-20 15:38:09 -0400

    moved selenium into right folder.

---
+++ src/test/selenium/Selenium2Example.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Selenium2Example {
+    public static void main(
+            String[] args) {
+        System.out.println(".dafsdf..");
+        // Create a new instance of the Firefox driver
+        // Notice that the remainder of the code relies on the interface,
+        // not the implementation.
+        //WebDriver driver = new FirefoxDriver();
+
+        // Close the browser
+        //driver.quit();
+    }
+}
