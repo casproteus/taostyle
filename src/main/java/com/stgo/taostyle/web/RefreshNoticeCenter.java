b6ffd07ffe2efd82b32ca6c64c853e04735405ba
tao <casproteus@hotmail.com>
2017-10-27 09:34:06 -0400

    added Math.abs in script when checking the location. and added null check to see if ther's client to broad cast.

--- src/main/java/com/stgo/taostyle/web/RefreshNoticeCenter.java
+++ src/main/java/com/stgo/taostyle/web/RefreshNoticeCenter.java
@@ -15,73 +15,84 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint(value = "/websocket/refreshNotice")
 public class RefreshNoticeCenter {
 
-	// private static final Log log = LogFactory.getLog(ChatAnnotation.class);
+    // private static final Log log = LogFactory.getLog(ChatAnnotation.class);
 
-	private static final String GUEST_PREFIX = "Guest";
-	private static final AtomicInteger connectionIds = new AtomicInteger(0);
-	private static final HashMap<String, CopyOnWriteArraySet<RefreshNoticeCenter>> connectionPoolMap = new HashMap<String, CopyOnWriteArraySet<RefreshNoticeCenter>>();
+    private static final String GUEST_PREFIX = "Guest";
+    private static final AtomicInteger connectionIds = new AtomicInteger(0);
+    private static final HashMap<String, CopyOnWriteArraySet<RefreshNoticeCenter>> connectionPoolMap =
+            new HashMap<String, CopyOnWriteArraySet<RefreshNoticeCenter>>();
 
-	private final String nickname;
-	private Session session;
+    private final String nickname;
+    private Session session;
 
-	public RefreshNoticeCenter() {
-		nickname = GUEST_PREFIX + connectionIds.getAndIncrement();
-	}
+    public RefreshNoticeCenter() {
+        nickname = GUEST_PREFIX + connectionIds.getAndIncrement();
+    }
 
-	@OnOpen
-	public void start(Session session) {
-		this.session = session;
-		String userNameStr = session.getUserPrincipal().getName();
-		int pos = userNameStr.indexOf('*');
-		String key = userNameStr.substring(pos + 1);
-		CopyOnWriteArraySet<RefreshNoticeCenter> set = connectionPoolMap.get(key);
-		if (set == null) {
-			set = new CopyOnWriteArraySet<RefreshNoticeCenter>();
-			connectionPoolMap.put(key, set);
-		}
-		if (!set.contains(this)) {
-			set.add(this);
-		}
-	}
+    @OnOpen
+    public void start(
+            Session session) {
+        this.session = session;
+        String userNameStr = session.getUserPrincipal().getName();
+        int pos = userNameStr.indexOf('*');
+        String key = userNameStr.substring(pos + 1);
+        CopyOnWriteArraySet<RefreshNoticeCenter> set = connectionPoolMap.get(key);
+        if (set == null) {
+            set = new CopyOnWriteArraySet<RefreshNoticeCenter>();
+            connectionPoolMap.put(key, set);
+        }
+        if (!set.contains(this)) {
+            set.add(this);
+        }
+    }
 
-	@OnClose
-	public void end() {
-		String userNameStr = session.getUserPrincipal().getName();
-		int pos = userNameStr.indexOf('*');
-		String key = userNameStr.substring(pos + 1);
-		CopyOnWriteArraySet<RefreshNoticeCenter> set = connectionPoolMap.get(key);
-		set.remove(this);
-	}
+    @OnClose
+    public void end() {
+        String userNameStr = session.getUserPrincipal().getName();
+        int pos = userNameStr.indexOf('*');
+        String key = userNameStr.substring(pos + 1);
+        CopyOnWriteArraySet<RefreshNoticeCenter> set = connectionPoolMap.get(key);
+        set.remove(this);
+    }
 
-	@OnMessage
-	public void incoming(String message) {
-	}
+    @OnMessage
+    public void incoming(
+            String message) {
+    }
 
-	@OnError
-	public void onError(Throwable t) throws Throwable {
-		String userNameStr = session.getUserPrincipal().getName();
-		int pos = userNameStr.indexOf('*');
-		String key = userNameStr.substring(pos + 1);
-		broadcast("something is wrong", key);
-	}
+    @OnError
+    public void onError(
+            Throwable t) throws Throwable {
+        String userNameStr = session.getUserPrincipal().getName();
+        int pos = userNameStr.indexOf('*');
+        String key = userNameStr.substring(pos + 1);
+        broadcast("something is wrong", key);
+    }
 
-	public static void broadcast(String msg, String personId) {
-		for (RefreshNoticeCenter client : connectionPoolMap.get(personId)) {
-			try {
-				synchronized (client) {
-					client.session.getBasicRemote().sendText(msg);
-				}
-			} catch (IOException e) {
-				// log.debug("Chat Error: Failed to send message to client", e);
-				connectionPoolMap.remove(client);
-				try {
-					client.session.close();
-				} catch (IOException e1) {
-					// Ignore
-				}
-				String message = String.format("* %s %s", client.nickname, "has been disconnected.");
-				broadcast(message, personId);
-			}
-		}
-	}
+    public static void broadcast(
+            String msg,
+            String personId) {
+        CopyOnWriteArraySet<RefreshNoticeCenter> set = connectionPoolMap.get(personId);
+        if (set == null || set.size() == 0) {
+            return;
+        }
+
+        for (RefreshNoticeCenter client : connectionPoolMap.get(personId)) {
+            try {
+                synchronized (client) {
+                    client.session.getBasicRemote().sendText(msg);
+                }
+            } catch (IOException e) {
+                // log.debug("Chat Error: Failed to send message to client", e);
+                connectionPoolMap.remove(client);
+                try {
+                    client.session.close();
+                } catch (IOException e1) {
+                    // Ignore
+                }
+                String message = String.format("* %s %s", client.nickname, "has been disconnected.");
+                broadcast(message, personId);
+            }
+        }
+    }
 }
