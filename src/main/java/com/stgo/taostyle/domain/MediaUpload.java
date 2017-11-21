0b19a6a7e7a32ca411aac1a090de7d1f6988730c
tao <casproteus@hotmail.com>
2017-11-20 21:52:49 -0500

    function imroved: make more stable, if there are multible file with same name, then instead of report exception, returning the latest one.

--- src/main/java/com/stgo/taostyle/domain/MediaUpload.java
+++ src/main/java/com/stgo/taostyle/domain/MediaUpload.java
@@ -65,10 +65,8 @@ public class MediaUpload {
             String pKey,
             Person person) {
         EntityManager tEntityManager = entityManager();
-        TypedQuery<Long> tQuery =
-                tEntityManager.createQuery(
-                        "SELECT COUNT(o) FROM MediaUpload AS o WHERE o.filepath LIKE :pKey and o.person = :person",
-                        Long.class);
+        TypedQuery<Long> tQuery = tEntityManager.createQuery(
+                "SELECT COUNT(o) FROM MediaUpload AS o WHERE o.filepath LIKE :pKey and o.person = :person", Long.class);
         tQuery.setParameter("pKey", pKey + "_%");
         tQuery.setParameter("person", person);
         long tCount = 0;
@@ -84,10 +82,8 @@ public class MediaUpload {
             String pKey,
             Person person) {
         EntityManager tEntityManager = entityManager();
-        TypedQuery<MediaUpload> tQuery =
-                tEntityManager.createQuery(
-                        "SELECT o FROM MediaUpload AS o WHERE o.filepath LIKE :pKey and o.person = :person",
-                        MediaUpload.class);
+        TypedQuery<MediaUpload> tQuery = tEntityManager.createQuery(
+                "SELECT o FROM MediaUpload AS o WHERE o.filepath LIKE :pKey and o.person = :person", MediaUpload.class);
         tQuery.setParameter("pKey", pKey + "_%");
         tQuery.setParameter("person", person);
         try {
@@ -102,10 +98,9 @@ public class MediaUpload {
             Person person) {
         EntityManager tEntityManager = entityManager();
         // note "ORDER BY o.filepath" is useless, because _10 will be infront of _2.
-        TypedQuery<String> tQuery =
-                tEntityManager.createQuery(
-                        "SELECT o.filepath FROM MediaUpload AS o WHERE o.filepath LIKE :pKey and o.person = :person",
-                        String.class);
+        TypedQuery<String> tQuery = tEntityManager.createQuery(
+                "SELECT o.filepath FROM MediaUpload AS o WHERE o.filepath LIKE :pKey and o.person = :person",
+                String.class);
         tQuery.setParameter("pKey", pKey + "_%");
         tQuery.setParameter("person", person);
         try {
@@ -123,14 +118,14 @@ public class MediaUpload {
         EntityManager tEntityManager = entityManager();
         tEntityManager.setFlushMode(FlushModeType.COMMIT);
         MediaUpload tObeFR;
-        TypedQuery<MediaUpload> tQuery =
-                tEntityManager.createQuery(
-                        "SELECT o FROM MediaUpload AS o WHERE o.filepath = :pKey and o.person =:person",
-                        MediaUpload.class);
+        TypedQuery<MediaUpload> tQuery = tEntityManager.createQuery(
+                "SELECT o FROM MediaUpload AS o WHERE o.filepath = :pKey and o.person =:person order by o.id",
+                MediaUpload.class);
         tQuery = tQuery.setParameter("pKey", key);
         tQuery = tQuery.setParameter("person", person);
         try {
-            tObeFR = tQuery.getSingleResult();
+            List<MediaUpload> mediaUploads = tQuery.getResultList();
+            tObeFR = mediaUploads.get(0);
         } catch (Exception e) {
             tObeFR = null;
         }
@@ -147,15 +142,13 @@ public class MediaUpload {
         TypedQuery<MediaUpload> tQuery;
         Customize customize = Customize.findCustomizeByKeyAndPerson(CC.app_ContentManager, person);
         if (customize != null && usr.getLoginname().toLowerCase().equals(customize.getCusValue().toLowerCase())) {
-            tQuery =
-                    tEntityManager.createQuery(
-                            "SELECT o FROM MediaUpload AS o WHERE o.publisher is not null ORDER BY o.submitDate DESC",
-                            MediaUpload.class);
+            tQuery = tEntityManager.createQuery(
+                    "SELECT o FROM MediaUpload AS o WHERE o.publisher is not null ORDER BY o.submitDate DESC",
+                    MediaUpload.class);
         } else {
-            tQuery =
-                    tEntityManager.createQuery(
-                            "SELECT o FROM MediaUpload AS o WHERE o.audient = :pKey ORDER BY o.submitDate DESC",
-                            MediaUpload.class);
+            tQuery = tEntityManager.createQuery(
+                    "SELECT o FROM MediaUpload AS o WHERE o.audient = :pKey ORDER BY o.submitDate DESC",
+                    MediaUpload.class);
             tQuery = tQuery.setParameter("pKey", usr);
         }
         try {
@@ -172,10 +165,9 @@ public class MediaUpload {
         EntityManager tEntityManager = entityManager();
         tEntityManager.setFlushMode(FlushModeType.COMMIT);
         List<MediaUpload> tObeFR;
-        TypedQuery<MediaUpload> tQuery =
-                tEntityManager.createQuery(
-                        "SELECT o FROM MediaUpload AS o WHERE o.publisher = :pKey ORDER BY o.submitDate DESC",
-                        MediaUpload.class);
+        TypedQuery<MediaUpload> tQuery = tEntityManager.createQuery(
+                "SELECT o FROM MediaUpload AS o WHERE o.publisher = :pKey ORDER BY o.submitDate DESC",
+                MediaUpload.class);
         tQuery = tQuery.setParameter("pKey", usr);
         try {
             tObeFR = tQuery.getResultList();
@@ -190,10 +182,8 @@ public class MediaUpload {
         EntityManager tEntityManager = entityManager();
         tEntityManager.setFlushMode(FlushModeType.COMMIT);
         List<MediaUpload> tObeFR;
-        TypedQuery<MediaUpload> tQuery =
-                tEntityManager.createQuery(
-                        "SELECT o FROM MediaUpload AS o WHERE o.person = :pKey ORDER BY o.submitDate DESC",
-                        MediaUpload.class);
+        TypedQuery<MediaUpload> tQuery = tEntityManager.createQuery(
+                "SELECT o FROM MediaUpload AS o WHERE o.person = :pKey ORDER BY o.submitDate DESC", MediaUpload.class);
         tQuery = tQuery.setParameter("pKey", person);
         try {
             tObeFR = tQuery.getResultList();
